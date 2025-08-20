e94cc9a2d3e2faee0021365ed97014dc19a61bdf
Aroshakalpana <89211301+Aroshakalpana2468@users.noreply.github.com>
2025-08-20 19:59:07 +0545

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "OEoEATyL#62fphFy6iE8Hjj6S4a36T9bwwbF8WGSZLj3A7p-oFDM", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94769974495",
